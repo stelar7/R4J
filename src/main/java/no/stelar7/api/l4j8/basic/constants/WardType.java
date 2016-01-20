0f2556c6848b890611965b1465f57684269e4eca
Steffen <dudedbz@gmail.com>
2016-01-20 22:52:06 +0100

    Add BLUE_TRINKET to wardTypes

--- src/main/java/no/stelar7/api/l4j8/basic/constants/WardType.java
+++ src/main/java/no/stelar7/api/l4j8/basic/constants/WardType.java
@@ -5,12 +5,14 @@ import java.util.stream.Stream;
 
 public enum WardType
 {
+    BLUE_TRINKET,
     SIGHT_WARD,
     TEEMO_MUSHROOM,
     UNDEFINED,
     VISION_WARD,
     YELLOW_TRINKET,
     YELLOW_TRINKET_UPGRADE;
+    
 
     /**
      * Returns an WardType from the provided code
