765e400cfc10781b43a68eb529dabcf36adcb009
Steffen <dudedbz@gmail.com>
2016-01-24 21:39:29 +0100

    Add a fake SecretFile, so maven does not complain.

---
+++ src/test/java/no/stelar7/api/l4j8/tests/SecretFile.java
@@ -0,0 +1,7 @@
+package no.stelar7.api.l4j8.tests;
+
+public class SecretFile
+{
+    public static final String API_KEY = "FAKE KEY";
+    public static final String TOURNAMENT_API_KEY = "FAKE KEY";
+}
