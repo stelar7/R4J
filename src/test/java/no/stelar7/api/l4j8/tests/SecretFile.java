cec2dd55c56fbf78d09db38f764a4cba01229615
stelar7 <dudedbz@gmail.com>
2016-05-15 14:02:30 +0200

    Add missing CREDS to the fake SecretFile

--- src/test/java/no/stelar7/api/l4j8/tests/SecretFile.java
+++ src/test/java/no/stelar7/api/l4j8/tests/SecretFile.java
@@ -2,6 +2,7 @@ package no.stelar7.api.l4j8.tests;
 
 public class SecretFile
 {
-    public static final String API_KEY = "FAKE KEY";
+    public static final String API_KEY            = "FAKE KEY";
     public static final String TOURNAMENT_API_KEY = "FAKE KEY";
+    public static final APICredentials CREDS      = new APICredentials(SecretFile.API_KEY, SecretFile.TOURNAMENT_KEY);
 }
