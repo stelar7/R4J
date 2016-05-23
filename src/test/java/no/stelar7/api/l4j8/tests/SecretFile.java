7b316f4db43adaa4273b629fd83ae75b670115b4
stelar7 <dudedbz@gmail.com>
2016-05-23 23:24:39 +0200

    Update SecretFile.java

--- src/test/java/no/stelar7/api/l4j8/tests/SecretFile.java
+++ src/test/java/no/stelar7/api/l4j8/tests/SecretFile.java
@@ -6,5 +6,5 @@ public class SecretFile
 {
     public static final String API_KEY            = "FAKE KEY";
     public static final String TOURNAMENT_API_KEY = "FAKE KEY";
-    public static final APICredentials CREDS      = new APICredentials(SecretFile.API_KEY, SecretFile.TOURNAMENT_KEY);
+    public static final APICredentials CREDS      = new APICredentials(SecretFile.API_KEY, SecretFile.TOURNAMENT_API_KEY);
 }
