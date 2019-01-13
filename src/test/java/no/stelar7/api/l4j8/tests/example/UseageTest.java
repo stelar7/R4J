96a684afd8ccfe14a2b4fbb17cd2429897697d6b
stelar7 <dudedbz@gmail.com>
2019-01-13 20:30:24 +0100

    Add perks to the usage example

--- src/test/java/no/stelar7/api/l4j8/tests/example/UseageTest.java
+++ src/test/java/no/stelar7/api/l4j8/tests/example/UseageTest.java
@@ -9,6 +9,7 @@ import no.stelar7.api.l4j8.pojo.championmastery.ChampionMastery;
 import no.stelar7.api.l4j8.pojo.match.*;
 import no.stelar7.api.l4j8.pojo.staticdata.champion.StaticChampion;
 import no.stelar7.api.l4j8.pojo.staticdata.mastery.StaticMastery;
+import no.stelar7.api.l4j8.pojo.staticdata.perk.*;
 import no.stelar7.api.l4j8.pojo.staticdata.rune.StaticRune;
 import no.stelar7.api.l4j8.pojo.summoner.Summoner;
 import no.stelar7.api.l4j8.tests.SecretFile;
@@ -22,12 +23,13 @@ public class UseageTest
     public void testUseage()
     {
         L4J8 api = new L4J8(SecretFile.CREDS);
-        DataCall.setLogLevel(LogLevel.DEBUG);
+        DataCall.setLogLevel(LogLevel.NONE);
         DataCall.setCacheProvider(new FileSystemCacheProvider());
         
         Map<Integer, StaticRune>     runeData      = api.getDDragonAPI().getRunes();
         Map<Integer, StaticMastery>  masteriesData = api.getDDragonAPI().getMasteries();
         Map<Integer, StaticChampion> championData  = api.getDDragonAPI().getChampions();
+        Map<Integer, StaticPerk>     perkData      = api.getDDragonAPI().getPerks();
         
         Summoner             stelar7        = new SummonerBuilder().withPlatform(Platform.EUW1).withName("stelar7").get();
         List<MatchReference> some           = stelar7.getGames().get();
@@ -38,6 +40,7 @@ public class UseageTest
         
         List<MatchRune>    runes     = self.getRunes();
         List<MatchMastery> masteries = self.getMasteries();
+        MatchPerks         perks     = self.getPerks();
         StaticChampion     champion  = championData.get(mostRecentGame.getChampionId());
         
         ChampionMastery mastery = stelar7.getChampionMastery(champion.getId());
@@ -58,20 +61,31 @@ public class UseageTest
         
         System.out.format("They laned against '%s' as '%s'%n", opponentIdentity.getSummonerName(), opponentChampion.getName());
         
-        System.out.format("%nThey used the following runes:%n");
-        for (MatchRune rune : runes)
+        if (!runes.isEmpty() && !masteries.isEmpty())
         {
-            String name = runeData.get(rune.getRuneId()).getName();
-            System.out.format("Name: '%-45s' Count: %s%n", name, rune.getRank());
-        }
-        
-        System.out.format("%nThey used the following masteries:%n");
-        for (MatchMastery matchMastery : masteries)
+            System.out.format("%nThey used the following runes:%n");
+            for (MatchRune rune : runes)
+            {
+                String name = runeData.get(rune.getRuneId()).getName();
+                System.out.format("Name: '%-45s' Count: %s%n", name, rune.getRank());
+            }
+            
+            System.out.format("%nThey used the following masteries:%n");
+            for (MatchMastery matchMastery : masteries)
+            {
+                String name = masteriesData.get(matchMastery.getMasteryId()).getName();
+                System.out.format("Name: '%-45s' Level: %s%n", name, matchMastery.getRank());
+            }
+        } else
         {
-            String name = masteriesData.get(matchMastery.getMasteryId()).getName();
-            System.out.format("Name: '%-45s' Level: %s%n", name, matchMastery.getRank());
+            System.out.format("%nThey used the following perks:%n");
+            for (MatchPerk perk : perks.getPerkList())
+            {
+                StaticPerk perkInfo = perkData.get(perk.getPerkId());
+                String     name     = perkInfo.getName();
+                System.out.format("Name: '%-20s' variables: %-5s, %-5s, %-5s%n", name, perk.getPerkVar1(), perk.getPerkVar2(), perk.getPerkVar3());
+            }
         }
-        
     }
     
     /*
@@ -100,6 +114,14 @@ public class UseageTest
     Name: 'Secret Stash                                 ' Level: 1
     Name: 'Merciless                                    ' Level: 5
     Name: 'Dangerous Game                               ' Level: 1
+    
+    They used the following perks:
+    Name: 'Fleet Footwork           ' variables: 725  , 0    , 0
+    Name: 'Triumph                  ' variables: 223  , 100  , 0
+    Name: 'Legend: Alacrity         ' variables: 24   , 0    , 0
+    Name: 'Coup de Grace            ' variables: 82   , 0    , 0
+    Name: 'Gathering Storm          ' variables: 14   , 0    , 0
+    Name: 'Nullifying Orb           ' variables: 510  , 0    , 0
 
      
      *
