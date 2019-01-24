132fd417bbacd02776f8c755295f18f9d1e68260
stelar7 <dudedbz@gmail.com>
2019-01-24 19:59:52 +0100

    Add date to logging

--- src/main/java/no/stelar7/api/l4j8/basic/constants/api/LogLevel.java
+++ src/main/java/no/stelar7/api/l4j8/basic/constants/api/LogLevel.java
@@ -1,5 +1,7 @@
 package no.stelar7.api.l4j8.basic.constants.api;
 
+import java.time.Instant;
+
 public enum LogLevel
 {
     NONE,
@@ -7,11 +9,12 @@ public enum LogLevel
     EXTENDED_INFO,
     DEBUG;
     
+    
     public void printIf(LogLevel other, String text)
     {
         if (other.ordinal() <= this.ordinal())
         {
-            System.err.printf("%s: %s%n", other.toString(), text);
+            System.err.printf("[%-30s - %-13s]: %s%n", Instant.now(), other.toString(), text);
         }
     }
 }
