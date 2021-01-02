29b5e885b770955c65ccd30b916027317b2a73cf
stelar7 <dudedbz@gmail.com>
2021-01-02 18:01:06 +0100

    Update valorant seasons

--- src/main/java/no/stelar7/api/r4j/basic/constants/types/val/Season.java
+++ src/main/java/no/stelar7/api/r4j/basic/constants/types/val/Season.java
@@ -8,9 +8,15 @@ import java.util.stream.Stream;
 public enum Season implements CodedEnum<Season>
 {
     NONE(""),
-    ACT_1("3f61c772-4560-cd3f-5d3f-a7ab5abda6b3"),
-    ACT_2("0530b9c4-4980-f2ee-df5d-09864cd00542"),
-    ACT_3("46ea6166-4573-1128-9cea-60a15640059b"),
+    CLOSED_BETA("0df5adb9-4dcb-6899-1306-3e9860661dd3"),
+    EPISODE_1("fcf2c8f4-4324-e50b-2e23-718e4a3ab046"),
+    EPISODE_1_ACT_1("3f61c772-4560-cd3f-5d3f-a7ab5abda6b3"),
+    EPISODE_1_ACT_2("0530b9c4-4980-f2ee-df5d-09864cd00542"),
+    EPISODE_1_ACT_3("46ea6166-4573-1128-9cea-60a15640059b"),
+    EPISODE_2("71c81c67-4fae-ceb1-844c-aab2bb8710fa"),
+    EPISODE_2_ACT_1("97b6e739-44cc-ffa7-49ad-398ba502ceb0"),
+    EPISODE_2_ACT_2("ab57ef51-4e59-da91-cc8d-51a5a2b9b8ff"),
+    EPISODE_2_ACT_3("52e9749a-429b-7060-99fe-4595426a0cf7"),
     ;
     
     
