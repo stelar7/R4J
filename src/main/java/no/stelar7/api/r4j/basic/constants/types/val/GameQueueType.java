f6c1241673b286f3f3ea3744db3ae9298d0fe0c3
spaykit <daniel@gankster.gg>
2023-05-01 12:16:59 +0300

    fix missing type

--- src/main/java/no/stelar7/api/r4j/basic/constants/types/val/GameQueueType.java
+++ src/main/java/no/stelar7/api/r4j/basic/constants/types/val/GameQueueType.java
@@ -5,9 +5,8 @@ import no.stelar7.api.r4j.basic.constants.types.CodedEnum;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public enum GameQueueType implements CodedEnum<GameQueueType>
-{
-    
+public enum GameQueueType implements CodedEnum<GameQueueType> {
+
     COMPETITIVE("competitive"),
     DEATHMATCH("deathmatch"),
     SPIKE_RUSH("spikerush"),
@@ -17,62 +16,54 @@ public enum GameQueueType implements CodedEnum<GameQueueType>
     ONE_FOR_ALL("onefa"),
     ESCALATION("ggteam"),
     NEW_MAP("newmap"),
-    SWIFTPLAY("swiftplay")
-    ;
-    
-    
+    SWIFTPLAY("swiftplay"),
+    PREMIER("premier");
+
     private final String queue;
-    
+
     /**
      * Constructor for MapType
      *
      * @param code the mapId
      */
-    GameQueueType(final String code)
-    {
+    GameQueueType(final String code) {
         this.queue = code;
     }
-    
+
     /**
      * Gets from code.
      *
      * @param mapId the map id
      * @return the from code
      */
-    public Optional<GameQueueType> getFromCode(final String mapId)
-    {
+    public Optional<GameQueueType> getFromCode(final String mapId) {
         return Stream.of(GameQueueType.values()).filter(t -> t.queue.equals(mapId)).findFirst();
     }
-    
+
     @Override
-    public String prettyName()
-    {
-        switch (this)
-        {
+    public String prettyName() {
+        switch (this) {
             default:
                 return "This enum does not have a pretty name";
         }
     }
-    
+
     /**
      * Gets id.
      *
      * @return the id
      */
-    public String getId()
-    {
+    public String getId() {
         return this.queue;
     }
-    
-    
+
     /**
      * Used internaly in the api...
      *
      * @return the value
      */
-    public String getValue()
-    {
+    public String getValue() {
         return getId();
     }
-    
+
 }
