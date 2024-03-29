0d2611058c6ac858531819577c7f138eca0fb92a
KaluNight <lucadasilva003@gmail.com>
2024-03-29 16:13:03 +0100

    Bug fix: SPECTATOR-V5 Api is still using old data format.
    v2 endpoint not working

--- src/test/java/no/stelar7/api/r4j/tests/lol/spectator/SpectatorV5Test.java
+++ src/test/java/no/stelar7/api/r4j/tests/lol/spectator/SpectatorV5Test.java
@@ -1,12 +1,15 @@
 package no.stelar7.api.r4j.tests.lol.spectator;
 
+import org.junit.jupiter.api.Test;
+
 import no.stelar7.api.r4j.basic.constants.api.regions.LeagueShard;
+import no.stelar7.api.r4j.basic.constants.api.regions.RegionShard;
+import no.stelar7.api.r4j.basic.constants.types.ApiKeyType;
 import no.stelar7.api.r4j.impl.R4J;
 import no.stelar7.api.r4j.impl.lol.raw.SpectatorV5API;
 import no.stelar7.api.r4j.pojo.lol.spectator.v5.SpectatorConnectionInformation;
-import no.stelar7.api.r4j.pojo.lol.summoner.Summoner;
+import no.stelar7.api.r4j.pojo.shared.RiotAccount;
 import no.stelar7.api.r4j.tests.SecretFile;
-import org.junit.jupiter.api.Test;
 
 public class SpectatorV5Test
 {
@@ -24,9 +27,9 @@ public class SpectatorV5Test
     {
         final R4J      r4J = new R4J(SecretFile.CREDS);
         SpectatorV5API api = r4J.getLoLAPI().getSpectatorV5API();
-        Summoner       sum = Summoner.byName(LeagueShard.EUW1, "stelar7");
+        RiotAccount    acc = r4J.getAccountAPI().getAccountByTag(RegionShard.EUROPE, "Incandescence33", "EUW", ApiKeyType.LOL);
         
-        SpectatorConnectionInformation currentGame = api.getCurrentGame(LeagueShard.EUW1, sum.getPUUID());
+        SpectatorConnectionInformation currentGame = api.getCurrentGame(LeagueShard.EUW1, acc.getPUUID());
         System.out.println(currentGame);
     }
 }
